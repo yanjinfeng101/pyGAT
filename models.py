81e0a246bf9bc537d7c1347cd2842edd22aed83a
yanjinfeng101 <58726560+yanjinfeng101@users.noreply.github.com>
2020-10-03 20:23:45 +0800

    Update models.py

--- models.py
+++ models.py
@@ -10,19 +10,20 @@ class GAT(nn.Module):
         #nheads表示有几个GAL层，最后拼接在一起
         super(GAT, self).__init__()
         self.dropout = dropout
-
+        
+        #定义multi-head的图注意力层
         self.attentions = [GraphAttentionLayer(nfeat, nhid, dropout=dropout, alpha=alpha, concat=True) for _ in range(nheads)]
         for i, attention in enumerate(self.attentions):
-            self.add_module('attention_{}'.format(i), attention)
+            self.add_module('attention_{}'.format(i), attention)#加入pytorch的Mouble模块
 
         self.out_att = GraphAttentionLayer(nhid * nheads, nclass, dropout=dropout, alpha=alpha, concat=False)
-
+#输出层
     def forward(self, x, adj):
         x = F.dropout(x, self.dropout, training=self.training)
-        x = torch.cat([att(x, adj) for att in self.attentions], dim=1)
+        x = torch.cat([att(x, adj) for att in self.attentions], dim=1)#每个head得到的表示进行拼接
         x = F.dropout(x, self.dropout, training=self.training)
         x = F.elu(self.out_att(x, adj))
-        return F.log_softmax(x, dim=1)
+        return F.log_softmax(x, dim=1)#log——max速度变快，保持数值稳定
 
 
 class SpGAT(nn.Module):
